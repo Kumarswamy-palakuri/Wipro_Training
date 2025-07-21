c4b44bd313f4f0259e7f4554fd92e37a879b38a7
Kumarswamy-palakuri <kumarqwe944@gmail.com>
2025-07-21 18:46:56 +0530

    first commit

---
+++ helloworld/HelloWorld/sum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class sum
+    {
+        static void Main()
+        {
+            int k = 10;
+            int p = 20;
+            Console.WriteLine(k+p);
+        }
+    }
+}

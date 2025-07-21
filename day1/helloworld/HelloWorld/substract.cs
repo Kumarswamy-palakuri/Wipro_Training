ee0d061ed968a84254f0122802657f672d970e50
Kumarswamy-palakuri <kumarqwe944@gmail.com>
2025-07-22 03:23:13 +0530

    added substract class

---
+++ day1/helloworld/HelloWorld/substract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class substract
+    {
+        static void Main()
+        {
+            int a = 10;
+            int b = 12;
+            int c = 22;
+            Console.WriteLine(a-b);
+            Console.WriteLine(a-c);
+            Console.WriteLine(b-c);
+        }
+    }
+}

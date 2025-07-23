0be50843c0a204c0923289029a428e3851ffa128
Kumarswamy-palakuri <kumarqwe944@gmail.com>
2025-07-23 22:30:30 +0530

    Added Boxing and SplitStrToWord programs

---
+++ day2/arrays/arrays/Boxing.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    internal class Boxing
+    {
+        static void Main()
+        {
+            int x = 54;
+            string str = "kumar";
+            double per = 89.25;
+
+            /* Boxing */
+            object ob1 = x;
+            object ob2 = str;
+            object ob3 = per;
+
+            //Console.WriteLine(ob1+" "+ob2+" "+ob3);
+            /* Unboxing */
+            int x1 = (int)ob1;
+            string str1 = (string)ob2;
+            double per1 = (double)ob3;
+
+            Console.WriteLine(x1+" "+str1+" "+per1);
+        }
+    }
+}
---
+++ day2/arrays/arrays/SplitStrToWord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    internal class SplitStrToWord
+    {
+        static void Main()
+        {
+            string str = Console.ReadLine();
+            string[] strArray = str.Split(' ');
+            int k = 0;
+            foreach(string i in strArray)
+            {
+                Console.WriteLine("{0} : {1}", (k++), i);
+                
+            }
+        }
+    }
+}

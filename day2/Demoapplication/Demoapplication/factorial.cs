5ca990d614e95e6309d1fba763c7ef74d48875fa
Kumarswamy-palakuri <kumarqwe944@gmail.com>
2025-07-23 22:55:43 +0530

    boxingex2

---
+++ day2/Demoapplication/Demoapplication/factorial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demoapplication
+{
+    internal class factorial
+    {
+        public int fact(int n)
+        {
+            int i = 1;
+            int k = 1;
+            while(i<=n)
+            {
+                k = k * i;
+                i++;
+            }
+            return k;
+        }
+        static void Main()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            factorial factorial1 = new factorial();
+            Console.WriteLine(factorial1.fact(n));
+        }
+    }
+}

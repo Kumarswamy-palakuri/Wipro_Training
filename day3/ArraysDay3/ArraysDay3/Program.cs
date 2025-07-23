7a216d1c2fb897b598026b4df87584a6d59ec1bf
Kumarswamy-palakuri <kumarqwe944@gmail.com>
2025-07-23 23:27:11 +0530

    day three 2d arrays

---
+++ day3/ArraysDay3/ArraysDay3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysDay3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] x = new int[2, 3]
+            {
+                {2,2,5 },
+                {5,8,4 }
+            };
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine(x[0, 0]);
+            Console.WriteLine(x[0, 1]);
+            Console.WriteLine(x[0, 2]);
+            Console.WriteLine("--------------------------------------");
+
+            Console.WriteLine(x[1, 0]);
+            Console.WriteLine(x[1, 1]);
+            Console.WriteLine(x[1, 2]);
+        
+}
+    }
+}

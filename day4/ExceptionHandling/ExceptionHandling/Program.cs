8db62bfe10a9f8e0f1a97b8db673926b056a0daf
Kumarswamy-palakuri <kumarqwe944@gmail.com>
2025-07-25 17:36:25 +0530

    daily practice

---
+++ day4/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter two numbers");
+            try
+            {
+                a = Convert.ToInt32(Console.ReadLine());
+                b = Convert.ToInt32(Console.ReadLine());
+                int res = a / b;
+                Console.WriteLine("Result is : "+res);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("you have entered too big number....");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("You tried to divide with zero.......");
+            }
+            finally
+            {
+                Console.WriteLine("Execution completed.");
+            }
+        }
+    }
+}
---
+++ day4/Extension/Extension/ExtentionLibrary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    internal class ExtentionLibrary
+    {
+        public void test1()
+        {
+            Console.WriteLine("this is test1");
+        }
+        public void test2()
+        {
+            Console.WriteLine("this is test2");
+        }
+        static void Main()
+        {
+
+        }
+    }
+}

34e8f636eed2b0a1c1ad4e382f5c951b5dab85f3
Kumarswamy-palakuri <kumarqwe944@gmail.com>
2025-07-27 10:07:53 +0530

    day 5 project completed

---
+++ day5/EmpPro/EmpProExceptions/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EmpProExceptions
+{
+    public class EmployException : Exception
+    {
+        public EmployException(string message) : base(message) { }
+    }
+}

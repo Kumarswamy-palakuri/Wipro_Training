b75b3b907e8c6f9b06a1c8151a4869c7b739ffbb
Kumarswamy-palakuri <kumarqwe944@gmail.com>
2025-07-29 20:51:40 +0530

    added new files as per 29-07-2025

---
+++ day6/AgentProject/AgentProject.Models/Agent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentProject.Models
+{
+    public class Agent
+    {
+        public int Agentid { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string City { get; set; }
+        public string Gender { get; set; }
+        public int PremiumAmount { get; set; }
+        public Agent() { }
+        public Agent (int agendid,string firstname,string lastname,string city,string gender,int premiumamount)
+        {
+            Agentid = agendid;
+            Firstname = firstname;
+            Lastname = lastname;
+            City = city;
+            Gender = gender;
+            PremiumAmount = premiumamount;
+        }
+    }
+}

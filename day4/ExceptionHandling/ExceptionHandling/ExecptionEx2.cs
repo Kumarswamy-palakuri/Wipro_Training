4a4ef0c90a29f60b2754aa0417464071cd3098ae
Kumarswamy-palakuri <kumarqwe944@gmail.com>
2025-07-29 20:51:40 +0530

    added new files as per 29-07-2025

---
+++ day4/ExceptionHandling/ExceptionHandling/ExecptionEx2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class ExecptionEx2
+    {
+        static void Main()
+        {
+
+            try
+            {
+                string str = Console.ReadLine();
+                int basic = int.Parse(Console.ReadLine());
+                Console.WriteLine("{0} is having {1}", str, basic);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("there is error occured -> "+e.Message);
+            }
+        }
+
+    }
+}

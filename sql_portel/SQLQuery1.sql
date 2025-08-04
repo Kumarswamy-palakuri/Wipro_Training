ecadb859cfe4076541246aa37f362f6ab6c154c5
kumaraswamy-palakuri <kumarqwe944@gmail.com>
2025-08-04 09:15:40 +0530

    add new files

---
+++ sql_portel/SQLQuery1.sql
@@ -0,0 +1,16 @@
+create database SampledB1;
+
+USE SampleDB1;
+
+
+create Table Accounts (
+Id Int,
+Name VARCHAR(100),
+Code int
+);
+
+select * from Accounts;
+
+insert into Accounts values(1,'kumar',0)
+
+insert into Accounts values(2,'Pavan',20);

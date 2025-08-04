5adf80f8187f05303f6d6770e932bee79ddcda35
kumaraswamy-palakuri <kumarqwe944@gmail.com>
2025-08-04 09:11:03 +0530

    add new files

---
+++ day4/ExceptionHandling/ExceptionHandling/CustomException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class CustomException
+    {
+        static void Main()
+        {
+            int age = int.Parse(Console.ReadLine());
+            if(age>18)
+            {
+                Console.WriteLine("Welcome to voter");
+            }
+            else
+            {
+                throw new CustException("get out from this place");
+            }
+        }
+        
+    }
+    public class CustException: Exception
+    {
+        public CustException(string message) : base(message)
+        { 
+        }
+
+        
+    }
+}
---
+++ day6/Table/Table/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Num that you want to print");
+            int n = int.Parse(Console.ReadLine());
+            for(int i=1;i<11;i++)
+            {
+                Console.WriteLine("{0}x{1}={2}",n,i,(n*i));
+            }
+        }
+    }
+}

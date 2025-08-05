00b533cbd09c42f094aa5d0be6842bec43a942b6
kumaraswamy-palakuri <kumarqwe944@gmail.com>
2025-08-05 22:09:06 +0530

    oops practice

---
+++ day8/OOPS/OOPS/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    internal class Employee
+    {
+        internal string name;
+        internal int id;
+        internal long mobile_num;
+        internal int age;
+        internal string city;
+        internal Employee()
+        { }
+        // ✅ Correct: constructor with no return type
+        internal Employee(string Name, int Id, long Mobile_num, int Age, string City)
+        {
+            name = Name;
+            id = Id;
+            mobile_num = Mobile_num;
+            age = Age;
+            city = City;
+        }
+    }
+
+}

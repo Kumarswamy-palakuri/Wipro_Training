3d6e823635e682feeaba09c568f438922ece8024
kumaraswamy-palakuri <kumarqwe944@gmail.com>
2025-08-15 15:50:20 +0530

    practice stdut data base

---
+++ Aug_13/StudentManagementAPI/StudentManagementAPI/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentManagementAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public List<Attendance> Attendances { get; set; }
+    }
+}

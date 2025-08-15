b984a8657323683127ac664e4f8e1000ef24266e
kumaraswamy-palakuri <kumarqwe944@gmail.com>
2025-08-15 15:50:20 +0530

    practice stdut data base

---
+++ Aug_13/StudentManagementAPI/StudentManagementAPI/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentManagementAPI.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+    }
+}
---
+++ Aug_13/StudentManagementAPI/StudentManagementAPI/Models/Attendance.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StudentManagementAPI.Models
+{
+    public class Attendance
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsPresent { get; set; }
+        public string Remarks { get; set; }
+
+        public int StudentId { get; set; }
+        [JsonIgnore]
+        public Student? Student { get; set; }
+    }
+}
